8b022be56bda8b10842b773c6e1fbb07ddae5e9e
Reto Achermann <reto.achermann@inf.ethz.ch>
2020-07-17 14:13:32 +0200

    cleanq/simpleq: adding proofs that enq/deq do not alter the state when full/empty
    Signed-off-by: Reto Achermann <reto.achermann@inf.ethz.ch>

--- CleanQ/CleanQ_SimpleQ.thy
+++ CleanQ/CleanQ_SimpleQ.thy
@@ -126,8 +126,6 @@ context CleanQ_SimpleQ begin
 
 (* Ring Buffer C parser output. *)
 thm rb_init_body_def
-thm c_fun_rb_can_enq_def
-thm c_fun_rb_init_def
 thm rb_can_enq_body_def
 thm rb_can_enq_c_fun_def
 thm rb_can_enq_def
@@ -142,13 +140,38 @@ term rb_C
 term buffer_C
 term head_C
 
+term ring_C
+
+
+(* some helper lemmas with the 64-bit words *)
+
+lemma unat_64word_eq_lt_lt:
+ "unat (C :: 64 word) = ULONG_MAX \<Longrightarrow> (D :: 64 word) < C \<Longrightarrow> unat (D) < ULONG_MAX"
+  by(simp add: word_less_nat_alt)
+
+lemma unat_64word_lt_lt_lt:
+ "unat (C :: 64 word) < ULONG_MAX \<Longrightarrow> (D :: 64 word) < C \<Longrightarrow> unat (D) < ULONG_MAX"
+  by(simp add: word_less_nat_alt)
+
+lemma unat_64word_leq_lt_lt: 
+  "unat (C :: 64 word) \<le> ULONG_MAX \<Longrightarrow> (D :: 64 word) < C \<Longrightarrow> unat (D) < ULONG_MAX"
+  using unat_64word_eq_lt_lt unat_64word_lt_lt_lt PackedTypes.order_leE 
+  by blast
+
+lemma unat_64word_lt_succ_leq:
+  "unat (C :: 64 word) \<le> ULONG_MAX \<Longrightarrow> (D :: 64 word) < C \<Longrightarrow> Suc (unat (D)) \<le> ULONG_MAX"
+  using unat_64word_leq_lt_lt  by (simp add: less_eq_Suc_le) 
+
+
+
 
 (* this one should be the pre-condition for the operation *)
 definition rb_valid_c :: " lifted_globals \<Rightarrow> rb_C ptr \<Rightarrow> bool"
   where "rb_valid_c s rb \<longleftrightarrow> is_valid_rb_C s rb 
                             \<and> unat (size_C (heap_rb_C s rb)) > 1 
                             \<and> unat (head_C (heap_rb_C s rb)) < unat(size_C (heap_rb_C s rb))
-                            \<and> unat (tail_C (heap_rb_C s rb)) < unat(size_C (heap_rb_C s rb))"
+                            \<and> unat (tail_C (heap_rb_C s rb)) < unat(size_C (heap_rb_C s rb)) 
+                            \<and> (\<forall>i <  unat (size_C (heap_rb_C s rb)). is_valid_buffer_C s (ring_C (heap_rb_C s rb) +\<^sub>p i))"
 
 
 definition
@@ -157,8 +180,8 @@ definition
   "the_rb s b i =  \<lparr> base = unat (offset_C (heap_buffer_C s (b  +\<^sub>p i))),
                      length = unat (length_C (heap_buffer_C s (b  +\<^sub>p i))) \<rparr>"
 
-definition rb_C_to_CleanQ_RB :: "lifted_globals \<Rightarrow> rb_C ptr \<Rightarrow> buffer  CleanQ_RB"
-  where "rb_C_to_CleanQ_RB s rb = \<lparr>  ring = \<lambda>i. the_rb s (ring_C (heap_rb_C s rb)) i,
+definition rb_C_to_CleanQ_RB :: "lifted_globals \<Rightarrow> rb_C ptr \<Rightarrow> buffer_C  CleanQ_RB"
+  where "rb_C_to_CleanQ_RB s rb = \<lparr>  ring = \<lambda>i. heap_buffer_C s ((ring_C (heap_rb_C s rb))  +\<^sub>p i),
                                      head =  unat (head_C (heap_rb_C s rb)), 
                                      tail =  unat (tail_C (heap_rb_C s rb)),
                                      size =  unat (size_C (heap_rb_C s rb)) \<rparr>"
@@ -175,22 +198,6 @@ lemma c_can_deq:
   by(auto)
 
 
-lemma foo1:
- "unat (C :: 64 word) = ULONG_MAX \<Longrightarrow> (D :: 64 word) < C \<Longrightarrow> unat (D) < ULONG_MAX"
-  by(simp add: word_less_nat_alt)
-
-lemma foo2:
- "unat (C :: 64 word) < ULONG_MAX \<Longrightarrow> (D :: 64 word) < C \<Longrightarrow> unat (D) < ULONG_MAX"
-  by(simp add: word_less_nat_alt)
-
-lemma foo3: "unat (C :: 64 word) \<le> ULONG_MAX \<Longrightarrow> (D :: 64 word) < C \<Longrightarrow> unat (D) < ULONG_MAX"
-  using foo1 foo2 PackedTypes.order_leE 
-  by blast
-
-lemma foo4:
-  "unat (C :: 64 word) \<le> ULONG_MAX \<Longrightarrow> (D :: 64 word) < C \<Longrightarrow> Suc (unat (D)) \<le> ULONG_MAX"
-  using foo3
-  by (simp add: less_eq_Suc_le) 
   
 
 
@@ -205,7 +212,7 @@ lemma c_can_enq:
   apply(wp) 
   unfolding rb_can_enq_def rb_full_def rb_C_to_CleanQ_RB_def 
   apply(simp add:rb_valid_c_def)
-  using foo4 word_less_nat_alt by fastforce
+  using unat_64word_lt_succ_leq word_less_nat_alt by fastforce
 
 
 lemma c_rb_enq:
@@ -232,38 +239,316 @@ lemma c_rb_enq2:
    \<lbrace> \<lambda>s. rb_valid_c s rb \<and> s0 = s  \<rbrace>
    rb_enq_c_fun rb b
    \<lbrace> \<lambda> r s. r \<noteq> 0 \<and> s = s0 \<rbrace>!"
-   apply(subst rb_enq_c_fun_def)
-   apply(auto)
-    apply(wp)
-   apply(simp)
-   apply(subst full)
-   apply(wp)
-  by(simp)
+   apply(simp add:rb_enq_c_fun_def)
+   apply(wp, simp add:full)+
+   done
+
   
-lemma c_rb_enq3:
+
+
+
+
+lemma c_rb_enq4:
   fixes rb :: "rb_C ptr" and b :: buffer_C and s0 :: lifted_globals
+  assumes space: "rb_can_enq_c_fun rb =  return (1)" and prev: "rb0 = rb_C_to_CleanQ_RB s rb"
+  shows "
+   \<lbrace> \<lambda>s. rb_valid_c s rb \<and> s0 = s  \<rbrace>
+   rb_enq_c_fun rb b
+   \<lbrace> \<lambda> r s. r =0 \<and> s \<noteq> s0 \<and> rb_C_to_CleanQ_RB s rb = rb_enq b rb0  \<rbrace>!"
+  apply(simp add:rb_enq_c_fun_def)
+  apply(wp)
+  apply(simp add:space)
+   apply(wp_once)
+  apply(auto)
+      prefer 5 apply(simp add:rb_valid_c_def)
+  prefer 4 apply(simp add:rb_valid_c_def)
+  apply (metis int_unat)
+    prefer 3 apply(simp add:rb_valid_c_def)
+   prefer 2 apply(simp add:rb_enq_def rb_C_to_CleanQ_RB_def prev) 
+  apply(simp add:rb_incr_head_def rb_write_def) apply(auto)
+  
+  sorry
+
+
+
+
+(* ==================================================================================== *)
+subsection \<open>Enqueue Operation\<close>
+(* ==================================================================================== *)
+
+text \<open>
+  For the enqueue operation, we can show that the operation preserves the size of the
+  ring buffer, and the tail pointer.
+\<close>
+
+lemma c_rb_enq_preserves_size :
+  fixes rb :: "rb_C ptr"
+  shows
+  "\<lbrace> \<lambda>s. rb_valid_c s rb \<and> s0 = s \<rbrace>
+   rb_enq_c_fun rb b
+   \<lbrace> \<lambda> r s. size_C (heap_rb_C s rb) = size_C (heap_rb_C s0 rb)  \<rbrace>!"
+   apply(simp add:rb_enq_c_fun_def)
+  apply(wp_once)+
+  apply(auto simp:rb_can_enq_c_fun_def)
+  apply(wp_once)+
+  apply(auto simp:rb_valid_c_def) 
+  apply (metis int_unat)
+  apply (simp add: unat_64word_leq_lt_lt Suc_leI word_less_nat_alt)
+  done
+
+lemma c_rb_enq_preserves_size2 :
+  fixes rb :: "rb_C ptr"
+  shows
+  "\<lbrace> \<lambda>s. rb_valid_c s rb \<and> size_C (heap_rb_C s rb) = k \<rbrace>
+   rb_enq_c_fun rb b
+   \<lbrace> \<lambda> r s. size_C (heap_rb_C s rb) = k  \<rbrace>!"
+   apply(simp add:rb_enq_c_fun_def)
+  apply(wp_once)+
+  apply(auto simp:rb_can_enq_c_fun_def)
+  apply(wp_once)+
+  apply(auto simp:rb_valid_c_def) 
+  apply (metis int_unat)
+  apply (simp add: unat_64word_leq_lt_lt Suc_leI word_less_nat_alt)
+  done
+
+
+lemma c_rb_enq_preserves_tail :
+  fixes rb :: "rb_C ptr"
+  shows
+  "\<lbrace> \<lambda>s. rb_valid_c s rb \<and> s0 = s \<rbrace>
+   rb_enq_c_fun rb b
+   \<lbrace> \<lambda> r s. tail_C (heap_rb_C s rb) = tail_C (heap_rb_C s0 rb)  \<rbrace>!"
+   apply(simp add:rb_enq_c_fun_def)
+  apply(wp_once)+
+  apply(auto simp:rb_can_enq_c_fun_def)
+  apply(wp_once)+
+  apply(auto simp:rb_valid_c_def) 
+  apply (metis int_unat)
+  apply (simp add: unat_64word_leq_lt_lt Suc_leI word_less_nat_alt)
+  done
+
+lemma c_rb_enq_preserves_tail2 :
+  fixes rb :: "rb_C ptr"
+  shows
+  "\<lbrace> \<lambda>s. rb_valid_c s rb \<and>  tail_C (heap_rb_C s rb) = k \<rbrace>
+   rb_enq_c_fun rb b
+   \<lbrace> \<lambda> r s.  tail_C (heap_rb_C s rb) = k  \<rbrace>!"
+   apply(simp add:rb_enq_c_fun_def)
+  apply(wp_once)+
+  apply(auto simp:rb_can_enq_c_fun_def)
+  apply(wp_once)+
+  apply(auto simp:rb_valid_c_def) 
+  apply (metis int_unat)
+  apply (simp add: unat_64word_leq_lt_lt Suc_leI word_less_nat_alt)
+  done
+
+
+text \<open>
+  If the queue is full, and the enqueue operation cannot put another buffer into
+  the ring, then the ring buffer remains in the same state. .
+\<close>
+
+
+lemma c_rb_enq_preserves_head :
+  fixes rb :: "rb_C ptr"
+  shows
+  "\<lbrace> \<lambda>s. rb_valid_c s rb \<and> s0 = s \<and> rb_can_enq_c_fun rb = return (0)  \<rbrace>
+   rb_enq_c_fun rb b
+   \<lbrace> \<lambda> r s. head_C (heap_rb_C s rb) = head_C (heap_rb_C s0 rb)  \<rbrace>!"
+   apply(simp add:rb_enq_c_fun_def)
+   apply(wp_once)+
+   apply(smt hoare_assume_preNF validNF_chain validNF_return)
+   done
+
+lemma c_rb_enq_preserves_head2 :
+  fixes rb :: "rb_C ptr"
+  shows
+  "\<lbrace> \<lambda>s. rb_valid_c s rb \<and> head_C (heap_rb_C s rb) = k \<and> rb_can_enq_c_fun rb = return (0) \<rbrace>
+   rb_enq_c_fun rb b
+   \<lbrace> \<lambda> r s. head_C (heap_rb_C s rb) = k \<rbrace>!"
+   apply(simp add:rb_enq_c_fun_def)
+   apply(wp_once)+
+   apply(smt hoare_assume_preNF validNF_chain validNF_return)
+   done
+
+lemma c_rb_enq_preserves_ring :
+  fixes rb :: "rb_C ptr"
+  shows
+  "\<lbrace> \<lambda>s. rb_valid_c s rb \<and> s0 = s \<and> rb_can_enq_c_fun rb = return (0)  \<rbrace>
+   rb_enq_c_fun rb b
+   \<lbrace> \<lambda> r s. ring_C (heap_rb_C s rb) = ring_C (heap_rb_C s0 rb)  \<rbrace>!"
+   apply(simp add:rb_enq_c_fun_def)
+   apply(wp_once)+
+   apply(smt hoare_assume_preNF validNF_chain validNF_return)
+   done
+
+
+lemma c_rb_enq_preserves_ring2 :
+  fixes rb :: "rb_C ptr"
+  shows
+  "\<lbrace> \<lambda>s. rb_valid_c s rb \<and> ring_C (heap_rb_C s rb) = k \<and> rb_can_enq_c_fun rb = return (0) \<rbrace>
+   rb_enq_c_fun rb b
+   \<lbrace> \<lambda> r s. ring_C (heap_rb_C s rb) = k  \<rbrace>!"
+   apply(simp add:rb_enq_c_fun_def)
+   apply(wp_once)+
+   apply(smt hoare_assume_preNF validNF_chain validNF_return)
+   done
+
+text \<open>
+  In fact, if the ring buffer is full, then the state is not altered at all.
+\<close>
+
+lemma c_rb_enq_full_preserves_state:
+  fixes rb :: "rb_C ptr" and b :: buffer_C
   shows "
    \<lbrace> \<lambda>s. rb_valid_c s rb \<and> s0 = s \<and> rb_can_enq_c_fun rb =  return (0)  \<rbrace>
    rb_enq_c_fun rb b
    \<lbrace> \<lambda> r s. r \<noteq> 0 \<and> s = s0 \<rbrace>!"
-  apply(subst rb_enq_c_fun_def)
-  apply(wp)
-  apply(auto)
-  apply(subst rb_can_enq_c_fun_def)
-  apply(wp)
-  apply(auto)
-  
-  by(simp)
+   apply(simp add:rb_enq_c_fun_def)
+  apply(wp_once)+
+  apply(simp)
+  apply (smt hoare_assume_preNF validNF_chain validNF_return)
+  done
 
 
-lemma "unat (C) \<le> ULONG_MAX \<Longrightarrow> D < C \<Longrightarrow> unat (D) < ULONG_MAX"
+
+(* ==================================================================================== *)
+subsection \<open>Dequeue Operation\<close>
+(* ==================================================================================== *)
+
+text \<open>
+  For the dequeue operation, we can show that the operation preserves the size of the
+  ring buffer, and the head pointer.
+\<close>
+
+lemma c_rb_deq_preserves_size :
+  fixes rb :: "rb_C ptr"
+  shows
+  "\<lbrace> \<lambda>s. rb_valid_c s rb \<and> s0 = s \<and>  is_valid_buffer_C s b \<rbrace>
+   rb_deq_c_fun rb b
+   \<lbrace> \<lambda> r s. size_C (heap_rb_C s rb) = size_C (heap_rb_C s0 rb) \<rbrace>!"
+   apply(simp add:rb_deq_c_fun_def)
+  apply(wp_once)+
+  apply(auto simp:rb_can_deq_c_fun_def)
+  apply(wp_once)+
+  apply(auto simp:rb_valid_c_def) 
+  apply (metis int_unat)
+  done
 
 
-  
+lemma c_rb_deq_preserves_size2 :
+  fixes rb :: "rb_C ptr"
+  shows
+  "\<lbrace> \<lambda>s. rb_valid_c s rb \<and> size_C (heap_rb_C s rb) = k  \<and>  is_valid_buffer_C s b \<rbrace>
+   rb_deq_c_fun rb b
+   \<lbrace> \<lambda> r s. size_C (heap_rb_C s rb) = k  \<rbrace>!"
+   apply(simp add:rb_deq_c_fun_def)
+  apply(wp_once)+
+  apply(auto simp:rb_can_deq_c_fun_def)
+  apply(wp_once)+
+  apply(auto simp:rb_valid_c_def)  
+  apply (metis int_unat)
+  done
 
-  
+
+lemma c_rb_deq_preserves_head :
+  fixes rb :: "rb_C ptr"
+  shows
+  "\<lbrace> \<lambda>s. rb_valid_c s rb \<and> s0 = s  \<and>  is_valid_buffer_C s b \<rbrace>
+   rb_deq_c_fun rb b
+   \<lbrace> \<lambda> r s. head_C (heap_rb_C s rb) = head_C (heap_rb_C s0 rb)  \<rbrace>!"
+   apply(simp add:rb_deq_c_fun_def)
+  apply(wp_once)+
+  apply(auto simp:rb_can_deq_c_fun_def)
+  apply(wp_once)+
+  apply(auto simp:rb_valid_c_def) 
+  apply (metis int_unat)
+  done
+
+lemma c_rb_deq_preserves_head2 :
+  fixes rb :: "rb_C ptr"
+  shows
+  "\<lbrace> \<lambda>s. rb_valid_c s rb \<and>  head_C (heap_rb_C s rb) = k  \<and> is_valid_buffer_C s b \<rbrace>
+   rb_deq_c_fun rb b
+   \<lbrace> \<lambda> r s.  head_C (heap_rb_C s rb) = k  \<rbrace>!"
+   apply(simp add:rb_deq_c_fun_def)
+  apply(wp_once)+
+  apply(auto simp:rb_can_deq_c_fun_def)
+  apply(wp_once)+
+  apply(auto simp:rb_valid_c_def)  
+  apply (metis int_unat)
+  done
 
 
+text \<open>
+  If the queue is empty, and the dequeue operation cannot pull another buffer from
+  the ring, then the ring buffer remains in the same state. .
+\<close>
+
+
+lemma c_rb_deq_preserves_tail :
+  fixes rb :: "rb_C ptr"
+  shows
+  "\<lbrace> \<lambda>s. rb_valid_c s rb \<and> s0 = s \<and> rb_can_deq_c_fun rb = return (0)  \<and> is_valid_buffer_C s b \<rbrace>
+   rb_deq_c_fun rb b
+   \<lbrace> \<lambda> r s. tail_C (heap_rb_C s rb) = tail_C (heap_rb_C s0 rb)  \<rbrace>!"
+   apply(simp add:rb_deq_c_fun_def)
+   apply(wp_once)+
+   apply(smt hoare_assume_preNF validNF_chain validNF_return)
+   done
+
+lemma c_rb_deq_preserves_tail2 :
+  fixes rb :: "rb_C ptr"
+  shows
+  "\<lbrace> \<lambda>s. rb_valid_c s rb \<and> tail_C (heap_rb_C s rb) = k \<and> rb_can_deq_c_fun rb = return (0)  \<and> is_valid_buffer_C s b \<rbrace>
+   rb_deq_c_fun rb b
+   \<lbrace> \<lambda> r s. tail_C (heap_rb_C s rb) = k \<rbrace>!"
+   apply(simp add:rb_deq_c_fun_def)
+   apply(wp_once)+
+   apply(smt hoare_assume_preNF validNF_chain validNF_return)
+   done
+
+lemma c_rb_deq_preserves_ring :
+  fixes rb :: "rb_C ptr"
+  shows
+  "\<lbrace> \<lambda>s. rb_valid_c s rb \<and> s0 = s \<and> rb_can_deq_c_fun rb = return (0) \<and> is_valid_buffer_C s b \<rbrace>
+   rb_deq_c_fun rb b
+   \<lbrace> \<lambda> r s. ring_C (heap_rb_C s rb) = ring_C (heap_rb_C s0 rb)  \<rbrace>!"
+   apply(simp add:rb_deq_c_fun_def)
+   apply(wp_once)+
+   apply(smt hoare_assume_preNF validNF_chain validNF_return)
+   done
+
+lemma c_rb_deq_preserves_ring2 :
+  fixes rb :: "rb_C ptr"
+  shows
+  "\<lbrace> \<lambda>s. rb_valid_c s rb \<and> ring_C (heap_rb_C s rb) = k \<and> rb_can_deq_c_fun rb = return (0)  \<and> is_valid_buffer_C s b \<rbrace>
+   rb_deq_c_fun rb b
+   \<lbrace> \<lambda> r s. ring_C (heap_rb_C s rb) = k  \<rbrace>!"
+   apply(simp add:rb_deq_c_fun_def)
+   apply(wp_once)+
+   apply(smt hoare_assume_preNF validNF_chain validNF_return)
+   done
+
+text \<open>
+  In fact, if the ring buffer is empty, then the state is not altered at all.
+\<close>
+
+lemma c_rb_deq_full_preserves_state:
+  fixes rb :: "rb_C ptr" and b :: buffer_C
+  shows "
+   \<lbrace> \<lambda>s. rb_valid_c s rb \<and> s0 = s \<and> rb_can_enq_c_fun rb =  return (0)  \<rbrace>
+   rb_enq_c_fun rb b
+   \<lbrace> \<lambda> r s. r \<noteq> 0 \<and> s = s0 \<rbrace>!"
+   apply(simp add:rb_enq_c_fun_def)
+  apply(wp_once)+
+  apply(simp)
+  apply (smt hoare_assume_preNF validNF_chain validNF_return)
+  done
+
+
+
+(* ####################################################### *)
 
 
 (* SimpleQ C parser output. *)
