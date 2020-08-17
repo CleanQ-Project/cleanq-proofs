7b2ec787ad8801009a09597a89eecba278c005a3
Roni Häcki <roni.haecki@inf.ethz.ch>
2020-08-17 11:15:58 +0200

    cleanq/crb: sampe proofs as SIMPLE but in COMPLEX
    Signed-off-by: Roni Häcki <roni.haecki@inf.ethz.ch>

---
+++ CleanQ/CleanQ_CRBModel_Complx.thy
@@ -0,0 +1,218 @@
+(*
+ * Copyright (c) 2020, CleanQ Project - Systems Group, ETH Zurich
+ * All rights reserved.
+ *
+ * This software may be distributed and modified according to the terms of
+ * the BSD 2-Clause license. Note that NO WARRANTY is provided.
+ *
+ * See "LICENSE" for details.
+ *
+ * SPDX-License-Identifier: BSD-2-Clause
+ *)
+
+
+
+section \<open>CRB proofs in Complex\<close>
+
+
+theory CleanQ_CRBModel_Complx
+(*<*) 
+  imports CleanQ_CRBModel
+          "../Complx/OG_Syntax"
+          "../Complx/OG_Hoare"
+(*>*)  
+begin
+
+(* ==================================================================================== *)
+subsection \<open>CleanQ Abstract Concurrent Ring Buffer Model State\<close>
+(* ==================================================================================== *)
+
+text \<open>
+  the model is exactly the same and we reuse the RB Model. 
+\<close>
+
+(*<*)
+(* Define some global variables to make Simpl/Complex proofs work *)
+record CleanQ_CRB_State_vars = 
+  CRB  :: "nat CleanQ_RB_State"
+  buf :: "nat"
+(*>*)
+
+
+
+(* ------------------------------------------------------------------------------------ *)
+subsubsection \<open>Hoare Triples for the Enqueue Operation\<close>
+(* ------------------------------------------------------------------------------------ *)
+
+
+text \<open>
+  We now show that the \verb+enqueue+ operation satisfies the pre and post conditions
+  for the predicates P, Q and R. 
+\<close>
+
+
+paragraph \<open>Writing the Head Element\<close>
+
+text \<open>
+  We show the Hoare triple with \verb+{P) write_head {Q}+.
+\<close>
+
+lemma  CleanQ_RB_write_head_x_hoare:
+  "\<Gamma>, \<Theta> |\<turnstile>\<^bsub>/ F\<^esub> 
+   \<lbrace> CleanQ_RB_enq_x_P K \<acute>CRB b \<rbrace> 
+    \<acute>CRB := (CleanQ_RB_write_head_x b \<acute>CRB)
+   \<lbrace> CleanQ_RB_enq_x_Q K \<acute>CRB b  \<rbrace>, \<lbrace>True\<rbrace>"     
+  apply(oghoare)
+  by simp
+  
+
+lemma  CleanQ_RB_write_head_y_hoare:
+ "\<Gamma>, \<Theta> |\<turnstile>\<^bsub>/ F\<^esub> 
+      \<lbrace> CleanQ_RB_enq_y_P K \<acute>CRB b \<rbrace> 
+        \<acute>CRB := (CleanQ_RB_write_head_y b \<acute>CRB)
+      \<lbrace> CleanQ_RB_enq_y_Q K \<acute> CRB b  \<rbrace>, \<lbrace>True\<rbrace>"                                                 
+  apply(oghoare)
+  by simp
+
+
+paragraph \<open>Incrementing the Head Pointer\<close>
+
+text \<open>
+  We show the Hoare triple with \verb+{Q) incr_head {R}+.
+\<close>
+
+lemma  CleanQ_RB_incr_head_x_hoare:
+   "\<Gamma>, \<Theta> |\<turnstile>\<^bsub>/ F\<^esub> 
+    \<lbrace> CleanQ_RB_enq_x_Q K \<acute>CRB b \<rbrace> 
+        \<acute>CRB := (CleanQ_RB_incr_head_x b \<acute>CRB)
+    \<lbrace> CleanQ_RB_enq_x_R K \<acute>CRB b  \<rbrace>, \<lbrace>True\<rbrace>"
+  apply(oghoare)
+  using CleanQ_RB_enq_x_inv_all CleanQ_RB_enq_x_result by fastforce
+
+lemma  CleanQ_RB_incr_head_y_hoare:
+  "\<Gamma>, \<Theta> |\<turnstile>\<^bsub>/ F\<^esub>  
+    \<lbrace>  CleanQ_RB_enq_y_Q K \<acute>CRB b \<rbrace> 
+        \<acute>CRB := (CleanQ_RB_incr_head_y b \<acute>CRB)
+    \<lbrace> CleanQ_RB_enq_y_R K \<acute>CRB b  \<rbrace>, \<lbrace>True\<rbrace>"
+  apply(oghoare)
+  using CleanQ_RB_enq_y_inv_all CleanQ_RB_enq_y_result by fastforce
+
+
+paragraph \<open>Full Enqueue Operation\<close>
+
+text \<open>
+  We show the Hoare triple with \verb+{P) enq {R}+.
+\<close>
+
+lemma CleanQ_RB_enq_x_hoare : 
+  "\<Gamma>, \<Theta> |\<turnstile>\<^bsub>/ F\<^esub>  
+   \<lbrace> CleanQ_RB_enq_x_P K \<acute>CRB b \<rbrace> 
+    \<acute>CRB := (CleanQ_RB_write_head_x b \<acute>CRB) ;;
+   \<lbrace> CleanQ_RB_enq_x_Q K \<acute>CRB b \<rbrace>
+     \<acute>CRB := (CleanQ_RB_incr_head_x b \<acute>CRB)
+   \<lbrace> CleanQ_RB_enq_x_R K \<acute>CRB b \<rbrace>, \<lbrace>True\<rbrace>"
+  apply(oghoare)
+  using CleanQ_RB_enq_x_inv_all CleanQ_RB_enq_x_result apply fastforce
+  by simp
+  
+
+lemma CleanQ_RB_enq_y_hoare : 
+  "\<Gamma>, \<Theta> |\<turnstile>\<^bsub>/ F\<^esub>  
+  \<lbrace> CleanQ_RB_enq_y_P K \<acute>CRB b \<rbrace> 
+    \<acute>CRB := (CleanQ_RB_write_head_y b \<acute>CRB) ;;
+  \<lbrace> CleanQ_RB_enq_y_Q K \<acute>CRB b \<rbrace>
+    \<acute>CRB := (CleanQ_RB_incr_head_y b \<acute>CRB)
+  \<lbrace> CleanQ_RB_enq_y_R K \<acute>CRB b \<rbrace>, \<lbrace>True\<rbrace>"
+  apply(oghoare)
+  using CleanQ_RB_enq_y_inv_all CleanQ_RB_enq_y_result apply fastforce
+  by simp
+
+
+(* ------------------------------------------------------------------------------------ *)
+subsubsection \<open>Hoare Triples for the Dequeue Operation\<close>
+(* ------------------------------------------------------------------------------------ *)
+
+
+text \<open>
+  We now show that the \verb+dequeue+ operation satisfies the pre and post conditions
+  for the predicates P, Q and R. 
+\<close>
+
+
+paragraph \<open>Reading the Tail Element\<close>
+
+text \<open>
+  We show the Hoare triple with \verb+{P) read_tail {Q}+.
+\<close>
+
+lemma  CleanQ_RB_read_tail_x_hoare:
+ "\<Gamma>, \<Theta> |\<turnstile>\<^bsub>/ F\<^esub>  
+  \<lbrace> CleanQ_RB_deq_x_P K \<acute>CRB \<rbrace> 
+    \<acute>buf := (CleanQ_RB_read_tail_x \<acute>CRB)
+  \<lbrace> CleanQ_RB_deq_x_Q K \<acute>CRB \<acute>buf  \<rbrace>, \<lbrace>True\<rbrace>"  
+  apply(oghoare)
+  by simp 
+
+lemma  CleanQ_RB_read_tail_y_hoare:
+  "\<Gamma>, \<Theta> |\<turnstile>\<^bsub>/ F\<^esub>  
+  \<lbrace> CleanQ_RB_deq_y_P K \<acute>CRB \<rbrace> 
+    \<acute>buf := (CleanQ_RB_read_tail_y \<acute>CRB)
+  \<lbrace> CleanQ_RB_deq_y_Q K \<acute>CRB \<acute>buf  \<rbrace>, \<lbrace>True\<rbrace>"  
+  apply(oghoare)  
+  by simp
+
+paragraph \<open>Incrementing the Tail Pointer\<close>
+
+text \<open>
+  We show the Hoare triple with \verb+{Q) incr_tail {R}+.
+\<close>
+
+lemma  CleanQ_RB_incr_tail_x_hoare:
+  "\<Gamma>, \<Theta> |\<turnstile>\<^bsub>/ F\<^esub>  
+    \<lbrace>  CleanQ_RB_deq_x_Q K \<acute>CRB b \<rbrace> 
+        \<acute>CRB := (CleanQ_RB_incr_tail_x b \<acute>CRB)
+    \<lbrace> CleanQ_RB_deq_x_R K \<acute>CRB b\<rbrace>,\<lbrace>True\<rbrace>" 
+  apply(oghoare)
+  using CleanQ_RB_deq_x_all_inv by fastforce
+
+
+lemma  CleanQ_RB_incr_tail_y_hoare:
+   "\<Gamma>, \<Theta> |\<turnstile>\<^bsub>/ F\<^esub> 
+    \<lbrace>  CleanQ_RB_deq_y_Q K \<acute>CRB b \<rbrace> 
+      \<acute>CRB := (CleanQ_RB_incr_tail_y b \<acute>CRB)
+    \<lbrace> CleanQ_RB_deq_y_R K \<acute> CRB b  \<rbrace>,\<lbrace>True\<rbrace>" 
+  apply(oghoare)
+  using CleanQ_RB_deq_y_all_inv by fastforce
+
+ 
+paragraph \<open>Full Dequeue Operation\<close>
+
+text \<open>
+  We show the Hoare triple with \verb+{P) deq {R}+.
+\<close>
+
+lemma CleanQ_RB_deq_x_hoare : 
+   "\<Gamma>, \<Theta> |\<turnstile>\<^bsub>/ F\<^esub>  
+    \<lbrace>  CleanQ_RB_deq_x_P K \<acute>CRB  \<rbrace> 
+      \<acute>buf := (CleanQ_RB_read_tail_x \<acute>CRB) ;;
+    \<lbrace> CleanQ_RB_deq_x_Q K \<acute>CRB \<acute>buf  \<rbrace>
+      \<acute>CRB := (CleanQ_RB_incr_tail_x \<acute>buf \<acute>CRB)
+    \<lbrace> CleanQ_RB_deq_x_R K \<acute>CRB \<acute>buf  \<rbrace>, \<lbrace>True\<rbrace>"
+  apply(oghoare)
+  using CleanQ_RB_deq_x_all_inv apply fastforce
+  by simp
+  
+
+lemma CleanQ_RB_deq_y_hoare : 
+   "\<Gamma>, \<Theta> |\<turnstile>\<^bsub>/ F\<^esub>   
+    \<lbrace>  CleanQ_RB_deq_y_P K \<acute>CRB  \<rbrace> 
+      \<acute>buf := (CleanQ_RB_read_tail_y \<acute>CRB) ;;
+    \<lbrace> CleanQ_RB_deq_y_Q K \<acute>CRB \<acute>buf  \<rbrace>
+      \<acute>CRB := (CleanQ_RB_incr_tail_y \<acute>buf \<acute>CRB)
+    \<lbrace> CleanQ_RB_deq_y_R K \<acute>CRB \<acute>buf  \<rbrace>, \<lbrace>True\<rbrace>"
+  apply(oghoare)
+  using CleanQ_RB_deq_y_all_inv apply fastforce
+  by simp
+
+
+end 
